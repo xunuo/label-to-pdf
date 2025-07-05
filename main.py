574ddc53e89df7768404ccf720fcdd5e96247b2d
许诺 <i@xunuo.com>
2025-07-05 18:42:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def annotate_image_to_pdf(img: Image.Image, annots: list, buf: BytesIO):
     bg_h             = font_size + 2 * padding
     box_fill_color   = parse_html_color("green", alpha=0.1)
     box_stroke_color = parse_html_color("green", alpha=0.2)
-    text_bg_color    = parse_html_color("green", alpha=0.3)
+    text_bg_color    = parse_html_color("green", alpha=0.4)
     text_bg_stroke_color   = parse_html_color("green", alpha=0.2)
                             
     for ann in annots:
