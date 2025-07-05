501b44274b754d111cd2af3b5f30bbd1fed47cfe
许诺 <i@xunuo.com>
2025-07-06 00:13:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -317,7 +317,7 @@ def download():
         timestamp = updated_at
 
     # 构造 PDF title metadata（含时间）
-    pdf_title = f"{project_title} / Task Id: {task_id} / Last Modified (Sydney Time): {timestamp}"
+    pdf_title = f"{project_title} / Task ID: {task_id} / Last Modified (Sydney Time): {timestamp}"
     # 构造下载文件名（不含时间）
     download_filename = f"{project_title}[task.{task_id}].pdf"
 
